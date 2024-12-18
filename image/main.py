10535d540f08e1fba2beb2320ceca562701a2d9b
ghost <email.global@example.com>
2024-12-18 14:38:07 -0500

    images

--- image/main.py
+++ image/main.py
@@ -2,7 +2,8 @@ from metadata_analyzer import extract_metadata
 
 
 if __name__ == "__main__":
-    filepath = "/home/kali/Documentos/Desarrollo/tools/image/descargas/Canon_PowerShot_S40.jpg"
+    #Colocar la ruta
+    filepath = ""
     metadata = extract_metadata(filepath)
     for key,value in metadata.items():
         print(f" {key} : {value}")
--- image/metadata_analyzer.py
+++ image/metadata_analyzer.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractclassmethod
 from PIL import Image
+import re
 import mimetypes
-from pdfminer.hight_level import extract_text
+from pdfminer.high_level import extract_text
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
+import docx
 
 
 class  MetadataExtractor(ABC):
@@ -43,12 +45,45 @@ class PdfMetadataExtractor(MetadataExtractor):
                               if isinstance(values,bytes):
                                    try:
                                         #Intentar decodificarlo en UTF-16be
-                                        decoded_value = value.decode('utf-16be')
-                                    except UnicodeDecodeError:
+                                        decode_value = values.decode('utf-16be')
+                                   except UnicodeDecodeError:
                                         #Intentar decoodificarlo en UTF-8
-                                        decode_value = value.decode('utf-8',errors='ignore')
-                                         
-    
+                                        decode_value = values.decode('utf-8',errors='ignore')
+                              else:
+                                   decode_value = values
+                              metadata[key] = decode_value
+               #Procesamo el texto del pdf para obtener otros datos de interes
+               text = extract_text(filepath)          
+               metadata['Emails'] = self._extract_emails(text)
+          return metadata
+          
+     def _extract_emails(self , text):
+          email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+          return re.findall(email_regex,text)
+     
+
+class DocxMetadataExtractor(MetadataExtractor):
+     def extract(self,filepath):
+          doc = docx.Document(filepath)
+          prop = doc.core_properties
+          attributes = ["author",
+                       "category",
+                       "comments",
+                       "content_status",
+                        "created",
+                        "identifier",
+                        "keywwords",
+                        "last_modified_by",
+                        "language",
+                        "modified",
+                        "subject",
+                        "title",
+                        "version"
+                       ]
+          metadata = { attr: getattr(prop, attr,None)for attr in attributes }
+          return metadata
+
+   
 class MetadataExtractorFactory:
      @staticmethod
      def get_extractor(filepath):
@@ -56,6 +91,12 @@ class MetadataExtractorFactory:
           if mime_type:
                if mime_type.startswith('image'):
                     return ImageMetadataExtractor()
+               if mime_type =='application/pdf':
+                    return PdfMetadataExtractor()
+               if mime_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                    return DocxMetadataExtractor
+          raise ValueError("Unsupported file type")
+               
 
 
 def extract_metadata(filepath):
